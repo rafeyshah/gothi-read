80a174029b11d4204f0df9cdd43f537f9b27998b
Abdul Rafey <dev.abdulrafey@gmail.com>
2026-02-21 00:35:23 +0100

    Removed uncessary files

--- tmp_one.py
+++
@@ -1,9 +0,0 @@
-import json
-from itertools import islice
-with open('configs/train_align.jsonl','r',encoding='utf-8') as f:
-    for line in f:
-        r=json.loads(line)
-        if r.get('ok_align') is True:
-            print(json.dumps(r)[:400])
-            print('keys', r.keys())
-            break
